0c3a3f51249ca9e7ffa899720de2e184d8e18180
iamakshat-dev <iamakshat2304@gmail.com>
2026-02-27 14:29:28 +0530

    UC1: Add welcome message module

---
+++ src/PalindromeCheckerApp.java
@@ -0,0 +1,7 @@
+public class PalindromeCheckerApp {
+    public static void main(String[] args){
+        System.out.print("Welcome to Palindrom Checker Mangement System \n");
+        System.out.print("Version: 1.0 \n");
+        System.out.print("System initialized successfully \n");
+    }
+}
